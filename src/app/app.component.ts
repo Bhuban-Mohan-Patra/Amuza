f51135c4e1a99768407afe23b38e0a2da11ceef4
Bhuban Mohan Patra <patrabhubanmohan@gmail.com>
2024-01-28 21:55:05 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink,NgbAlert,HeaderComponent,FooterComponent,LoginComponent,SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        component:HomeComponent,
+        path: ''
+        
+    },
+    {
+        component: AboutComponent,
+        path:'about'
+    },
+    {
+        component: ContactComponent,
+        path:'contact'
+    },
+    {
+        component: LoginComponent,
+        path:'login'
+    },
+    {
+        component: SignupComponent,
+        path:'signup'
+    }
+];
